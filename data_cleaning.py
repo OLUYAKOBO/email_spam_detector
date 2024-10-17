a760cd4ac2a5ca4a5711fd8a5dddebea1102e773
Yakub Junaid <Junaidyakub28@gmail.com>
2024-10-17 23:25:27 +0100

    This module was created to remove stop words from the emails

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 def clean_data(df):
     mail = ['Message']
+
+    """
+    This function is created to remove stop words from the email.
+    
+    
+    """
+
     for i in mail:
         df[i] = df[i].apply(lambda x : x.replace(',', ' '))
         df[i] = df[i].apply(lambda x: x.replace('!',''))
